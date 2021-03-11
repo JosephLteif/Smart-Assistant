7d1bbd58c716da227141057129a38bc2a4cf8895
Joseph Lteif <JosephLteif@outlook.com>
2021-03-11 09:31:10 +0200

    Face detection now detect multiple faces

--- FaceDetection.py
+++ FaceDetection.py
@@ -21,14 +21,15 @@ def funct():
 
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+        img = cv2.imread("VideoCapture.jpg")
         for (x, y, w, h) in faces:
-            check, frame = video.read()
-            img = cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = img[y:y+h, x:x+w]
             eyes = eye_cascade.detectMultiScale(roi_gray)
             for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+               cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+            
         if len(faces) == 0:
 
             cv2.imshow('img', np.fliplr(frame))
@@ -38,6 +39,7 @@ def funct():
             Key = cv2.waitKey(1)
         if Key == ord('q'):
             break
+        print("Found {0} Faces!".format(len(faces)))
 
     os.remove('VideoCapture.jpg')
 
