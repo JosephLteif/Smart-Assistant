597c532b421f93e4fe4446e73ad45895c65b588c
Joseph Lteif <JosephLteif@outlook.com>
2021-03-11 10:39:04 +0200

    Display text when a face is detected

--- FaceDetection.py
+++ FaceDetection.py
@@ -8,12 +8,15 @@ def funct():
         cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier(
         cv2.data.haarcascades + 'haarcascade_eye.xml')
-
+    side_face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + 'haarcascade_profileface.xml')
+    
     video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    img = None
+    font = cv2.FONT_HERSHEY_SIMPLEX
     while True:
         # check returns true if python can actually read and frame is ndim numpy array
         check, frame = video.read()
+        frame = np.fliplr(frame).copy()
         cv2.imwrite("VideoCapture.jpg", frame)
         
         image = cv2.imread("VideoCapture.jpg")
@@ -21,25 +24,34 @@ def funct():
 
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-        img = cv2.imread("VideoCapture.jpg")
-        for (x, y, w, h) in faces:
-            img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = img[y:y+h, x:x+w]
-            eyes = eye_cascade.detectMultiScale(roi_gray)
-            for (ex, ey, ew, eh) in eyes:
-               cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-            
+        if len(faces):
+            for (x, y, w, h) in faces:
+                image = cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = image[y:y+h, x:x+w]
+                cv2.putText(image,'Unknown',(x,y+h+20), font, 0.5, (255,0,0)) #---write the text
+                # eyes = eye_cascade.detectMultiScale(roi_gray)
+                # for (ex, ey, ew, eh) in eyes:
+                #    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        else:
+            sidefaces = side_face_cascade.detectMultiScale(gray, 1.3, 5)
+            if len(sidefaces):
+                print("Hello!")
+            for (x, y, w, h) in sidefaces:
+                image = cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = image[y:y+h, x:x+w]
+
         if len(faces) == 0:
 
-            cv2.imshow('img', np.fliplr(frame))
+            cv2.imshow('img', frame)
             Key = cv2.waitKey(1)
         else:
-            cv2.imshow('img', np.fliplr(img))
+            cv2.imshow('img', image)
             Key = cv2.waitKey(1)
         if Key == ord('q'):
             break
-        print("Found {0} Faces!".format(len(faces)))
+        # print("Found {0} Faces!".format(len(faces)))
 
     os.remove('VideoCapture.jpg')
 
