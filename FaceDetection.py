bc334ae00e1c3cb32c57c7d2fd7887ebfe062a34
Joseph Lteif <JosephLteif@outlook.com>
2021-03-11 16:12:31 +0200

    Fixed High CPU Usage in FD

--- FaceDetection.py
+++ FaceDetection.py
@@ -11,32 +11,40 @@ def funct():
     side_face_cascade = cv2.CascadeClassifier(
         cv2.data.haarcascades + 'haarcascade_profileface.xml')
     
-    video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    video = cv2.VideoCapture(0,cv2.CAP_DSHOW)
     font = cv2.FONT_HERSHEY_SIMPLEX
+    i = 0
     while True:
+        if i>3:
+            i=0
+        
         # check returns true if python can actually read and frame is ndim numpy array
         check, frame = video.read()
         frame = np.fliplr(frame).copy()
+        
         cv2.imwrite("VideoCapture.jpg", frame)
         
         image = cv2.imread("VideoCapture.jpg")
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+        if i==0:
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            sidefaces = side_face_cascade.detectMultiScale(gray, 1.3, 5)
+        
         for (x, y, w, h) in faces:
             State = 'Unknown - Neutral'
             image = cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 255), 2)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = image[y:y+h, x:x+w]
-            smiles = smile_cascade.detectMultiScale(roi_gray, 1.3, 43)
+            if i==0:
+                smiles = smile_cascade.detectMultiScale(roi_gray, 1.3, 43)
             for (ex, ey, ew, eh) in smiles:
-               cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 255, 255), 2)
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 255, 255), 2)
             if len(smiles):
                 State = 'Unknown - Happy'
             cv2.putText(image,State,(x,y+h+20), font, 0.5, (255,255,255)) #---write the text
-
-        sidefaces = side_face_cascade.detectMultiScale(gray, 1.3, 5)
+        
         for (x, y, w, h) in sidefaces:
             State = 'Unknown - Neutral'
             image = cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 255), 2)
@@ -44,7 +52,7 @@ def funct():
             roi_color = image[y:y+h, x:x+w]
             smiles = smile_cascade.detectMultiScale(roi_gray, 1.3, 43)
             for (ex, ey, ew, eh) in smiles:
-               cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 255, 255), 2)
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 255, 255), 2)
             if len(smiles):
                 State = 'Unknown - Happy'
             cv2.putText(image,State,(x,y+h+20), font, 0.5, (255,255,255)) #---write the text
@@ -59,7 +67,7 @@ def funct():
         if Key == ord('q'):
             break
         # print("Found {0} Faces!".format(len(faces)))
-
+        i = i + 1
     os.remove('VideoCapture.jpg')
 
 funct()
