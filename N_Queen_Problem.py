78dd8d6440a3951ffea8375a76a618bc056bb956
Joseph Lteif <JosephLteif@outlook.com>
2021-03-25 16:47:07 +0200

    Face_Detection Done with Visual N Queen

--- N_Queen_Problem.py
+++ N_Queen_Problem.py
@@ -1,13 +1,21 @@
+import cv2
+import operator
+from PIL import Image
 # Python3 program to solve N Queen  
 # Problem using backtracking 
 global N 
-N = 12
-  
-def printSolution(board): 
+N = 8
+ 
+def printSolution(board):
+    coord = dict()
     for i in range(N): 
-        for j in range(N): 
-            print (board[i][j], end = " ") 
-        print() 
+        for j in range(N):
+            point = board[i][j]
+            print (point, end = " ")
+            if point == 1:
+                coord[i] = j 
+        print()
+    return coord
   
 # A utility function to check if a queen can 
 # be placed on board[row][col]. Note that this 
@@ -64,6 +72,37 @@ def solveNQUtil(board, col):
     # if the queen can not be placed in any row in 
     # this colum col then return false 
     return False
+
+#This function is used to place the Queen images on the ChessBoard image
+def Place_Queens(Board, Queen, index_x, index_y):
+
+    # suppose img1 and img2 are your two images
+    img1 = Board
+    img2 = Queen
+
+    # suppose img2 is to be shifted by `shift` amount 
+    shift = (8+(index_x*27), 3+(index_y*27))
+
+    # compute the size of the panorama
+    x = map(operator.add, img2.size, shift)
+    nw, nh = map(max, x, img1.size)
+    print(nw)
+    print(nh)
+
+    # paste img1 on top of img2
+    newimg1 = Image.new('RGBA', size=(nw, nh), color=(0, 0, 0, 0))
+    newimg1.paste(img2, shift)
+    newimg1.paste(img1, (0, 0))
+
+    # paste img2 on top of img1
+    newimg2 = Image.new('RGBA', size=(nw, nh), color=(0, 0, 0, 0))
+    newimg2.paste(img1, (0, 0))
+    newimg2.paste(img2, shift)
+
+    # blend with alpha=0.5
+    result = Image.blend(newimg1, newimg2, alpha=0.5)
+    
+    return result
   
 # This function solves the N Queen problem using 
 # Backtracking. It mainly uses solveNQUtil() to 
@@ -74,24 +113,32 @@ def solveNQUtil(board, col):
 # solutions, this function prints one of the 
 # feasible solutions. 
 def solveNQ(): 
-    board = [ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ] 
-  
+    sub_Board
+    board = [ [0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0]]
+
     if solveNQUtil(board, 0) == False: 
         print ("Solution does not exist") 
         return False
-  
-    printSolution(board) 
+
+    coord = printSolution(board)
+    ChessBoardimg = Image.open("./Assets/ChessBoard.png")
+    Queenimg = Image.open("./Assets/Queen.png")
+    for i in coord:
+        ChessBoardimg = Place_Queens(ChessBoardimg, Queenimg, coord[i], i)
+        ChessBoardimg.show()
+        print("Enter")
+        cv2.waitKey(100)
+        print("close")
+        try:
+            ChessBoardimg.close()
+    cv2.waitKey(0)
     return True
   
 # Driver Code 
