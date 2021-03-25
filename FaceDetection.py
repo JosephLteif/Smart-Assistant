e13dc016601fa0d267ba130f6eb029d8ea462656
Joseph Lteif <JosephLteif@outlook.com>
2021-03-25 16:47:30 +0200

    Face_Detection done

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,97 +1,141 @@
-# imports
-import numpy as np
-import cv2
 import os
+import os.path
+from os import path
+import cv2
 import face_recognition
-# import threading
+import numpy as np
+import json
+import threading
 
-# encodings1 = 0
-
-# def encode(image, faces):
-#     encodings1 = face_recognition.face_encodings(image, faces)[0]
-#     return encodings1
-
-def funct():
-    face_cascade = cv2.CascadeClassifier(
+face_cascade = cv2.CascadeClassifier(
         cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    smile_cascade = cv2.CascadeClassifier(
-        cv2.data.haarcascades + 'haarcascade_smile.xml')
-    side_face_cascade = cv2.CascadeClassifier(
-        cv2.data.haarcascades + 'haarcascade_profileface.xml')
 
-    video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    i = 0
-    # Joseph_encoding = face_recognition.face_encodings(face_recognition.load_image_file("../Image for python FD/FaceImage0.jpg"))[0]
+def get_encoded_faces():
+    """
+    looks through the faces folder and encodes all
+    the faces
+
+    :return: dict of (name, image encoded)
+    """
+    encoded = {}
+    Edit = False
+    if not path.exists("face_Data.json"):
+        open('face_Data.json', "w").close()
+    with open('face_Data.json') as f:
+        try:
+            encoded = json.load(f)
+        except:
+            encoded = {}
+    Keys = encoded.keys()
+
+    for dirpath, dnames, fnames in os.walk("faces"):
+        for f in fnames:
+            if f.split(".")[0] not in Keys:
+                Edit = True
+                if f.endswith(".jpg") or f.endswith(".png"):
+                    face = face_recognition.load_image_file("faces\\" + f)
+                    encoding = face_recognition.face_encodings(face)[0]
+                    encoded[f.split(".")[0]] = encoding.tolist()
+    if Edit:
+        # Serializing json 
+        json_Data = json.dumps(encoded)
+
+        # Writing to face_Data.json
+        with open("face_Data.json", "w") as outfile:
+            outfile.write(json_Data)
+    return encoded
+
+
+def unknown_image_encoded(img, face_location):
+    """
+    encode a face given the image
+    """
+    return face_recognition.face_encodings(img, face_location)
+
+def locate_Face(img):
+    return face_recognition.face_locations(img)
+
+def setup_Video():
+    return cv2.VideoCapture(0)
+
+def Get_Frame(video):
+    return video.read()
+
+def classify_face():
+    """
+    will find all of the faces in a given image and label
+    them if it knows what they are
+
+    :param im: str of file path
+    :return: list of face names
+    """
+    video = setup_Video()
+    
+    faces = get_encoded_faces()
+    faces_encoded = list(faces.values())
+    known_face_names = list(faces.keys())
+    unknown_face_encodings = []
+    
+    check, frame = Get_Frame(video)
+    frame = np.fliplr(frame).copy()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    face_locations = face_recognition.face_locations(frame)
+
+    unknown_face_encodings = unknown_image_encoded(
+            frame, face_locations)
     while True:
-        if i > 3:
-            i = 0
-
-        # check returns true if python can actually read and frame is ndim numpy array
-        check, frame = video.read()
+        
+        found = False
+        
+        check0, frame0 = Get_Frame(video)
+        check, frame = Get_Frame(video)
         frame = np.fliplr(frame).copy()
-        frame = cv2.resize(frame, (0, 0), fx=0.75, fy=0.75)
-
+        
+        gray0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        face_locations0 = face_cascade.detectMultiScale(gray0, 1.3, 5)
+        face_locations1 = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-        if i == 0:
-            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-            # sidefaces = side_face_cascade.detectMultiScale(gray, 1.3, 5)
+        if len(face_locations0) < len(face_locations1):
+            found = True
+            img = cv2.resize(frame, (0,0), fx=1,fy=1)
+            face_locations1 = face_recognition.face_locations(img)
+            unknown_face_encodings = unknown_image_encoded(frame, face_locations1)
+        
+        face_names = []
+        name = ""
+        for face_encoding in unknown_face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(faces_encoded, face_encoding)
+            name = "Unknown"
 
-        for (x, y, w, h) in faces:
-            image = cv2.rectangle(
-                frame, (x, y), (x+w, y+h), (255, 255, 255), 2)            
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = image[y:y+h, x:x+w]
-            if i == 0:
-                smiles = smile_cascade.detectMultiScale(roi_gray, 1.3, 43)
-                # encodings1 = face_recognition.face_encodings(image, faces)[0]
-                # t1 = threading.Thread(target=encode, args=(image,faces), daemon=True)
-                # t1.start()
-                # t1.join()
-                # print(encodings1)
-            for (ex, ey, ew, eh) in smiles:
-                cv2.rectangle(roi_color, (ex, ey),
-                              (ex+ew, ey+eh), (255, 255, 255), 2)
-            # print(face_recognition.face_distance([encodings1],Joseph_encoding))
-            Name = "UnKnown"
-            # if(face_recognition.face_distance([encodings1],Joseph_encoding)<0.7):
-            #     Name = "Joseph Lteif"
-            # else:
-                # Name = "UnKnown"
-            if len(smiles):
-                State = '{} - Happy'.format(Name)
-            else:
-                State = '{} - Neutral'.format(Name)
-            cv2.putText(image, State, (x, y+h+20), font, 0.5,
-                        (255, 255, 255))  # ---write the text
+            # use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(
+                faces_encoded, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
 
-        # for (x, y, w, h) in sidefaces:
-        #     State = 'UnKnown - Neutral'
-        #     image = cv2.rectangle(
-        #         frame, (x, y), (x+w, y+h), (255, 255, 255), 2)
-        #     roi_gray = gray[y:y+h, x:x+w]
-        #     roi_color = image[y:y+h, x:x+w]
-        #     smiles = smile_cascade.detectMultiScale(roi_gray, 1.3, 43)
-        #     for (ex, ey, ew, eh) in smiles:
-        #         cv2.rectangle(roi_color, (ex, ey),
-        #                       (ex+ew, ey+eh), (255, 255, 255), 2)
-        #     if len(smiles):
-        #         State = 'Unknown - Happy'
-        #     cv2.putText(image, State, (x, y+h+20), font, 0.5,
-        #                 (255, 255, 255))  # ---write the text
+            face_names.append(name)
 
-        if len(faces) == 0:
-            frame = cv2.resize(frame, (0, 0), fx=1+(1/3), fy=1+(1/3))
-            cv2.imshow('img', frame)
-            Key = cv2.waitKey(1)
-        else:
-            image = cv2.resize(image, (0, 0), fx=1+(1/3), fy=1+(1/3))
-            cv2.imshow('img', image)
-            Key = cv2.waitKey(1)
-        if Key == ord('q'):
-            break
-        i = i + 1
+        if not found:    
+            for (x, y, w, h), name in zip(face_locations1, face_names):
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2) 
+                
+                    # Draw a label with a name below the face
+                    cv2.rectangle(frame, (x, y+h),(x+w, y+h+20), (255, 0, 0), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(frame, name, (x + 5, y+h + 15),font, 0.5, (255, 255, 255), 2)           
+
+            
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            return face_names
 
 
-funct()
+classify_face()
